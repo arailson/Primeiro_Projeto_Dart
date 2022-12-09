2f0a9dfba5d7b28ad68c68620e25e58a89abe761
Arailson <arailson.vieira@gmail.com>
2022-12-08 23:57:49 -0300

    Text e ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -11,75 +11,95 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.purple,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.pink,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.cyan,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.green,
-                  height: 50,
-                  width: 50,
-                ),
-                Container(
-                  color: Colors.yellow,
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            )
-          ],
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Container(
+                    color: Colors.purple,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    color: Colors.pink,
+                    width: 50,
+                    height: 50,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.cyan,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 50,
+                    width: 50,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    height: 50,
+                    width: 50,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.amber,
+                height: 30,
+                width: 300,
+                child: Text('Diamante Amarelo',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                    )),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('Você apertou o botão');
+                },
+                child: Text('Aperte o botão'),
+              ),
+            ],
+          )),
+    );
   }
 }
