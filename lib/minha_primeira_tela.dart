9b6e3ed8ed153c94a46fb6a6352e350aca61e7fc
Arailson <arailson.vieira@gmail.com>
2022-12-10 00:03:18 -0300

    Widget Scaffold

---
+++ lib/minha_primeira_tela.dart
@@ -0,0 +1,76 @@
+// Container(
+//           color: Colors.white,
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Stack(
+//                 alignment: AlignmentDirectional.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.red,
+//                     width: 100,
+//                     height: 100,
+//                   ),
+//                   Container(
+//                     color: Colors.blue,
+//                     width: 50,
+//                     height: 50,
+//                   )
+//                 ],
+//               ),
+//               Stack(
+//                 alignment: AlignmentDirectional.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.purple,
+//                     width: 100,
+//                     height: 100,
+//                   ),
+//                   Container(
+//                     color: Colors.pink,
+//                     width: 50,
+//                     height: 50,
+//                   )
+//                 ],
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                 crossAxisAlignment: CrossAxisAlignment.center,
+//                 children: [
+//                   Container(
+//                     color: Colors.cyan,
+//                     height: 50,
+//                     width: 50,
+//                   ),
+//                   Container(
+//                     color: Colors.green,
+//                     height: 50,
+//                     width: 50,
+//                   ),
+//                   Container(
+//                     color: Colors.yellow,
+//                     height: 50,
+//                     width: 50,
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 color: Colors.amber,
+//                 height: 30,
+//                 width: 300,
+//                 child: Text('Diamante Amarelo',
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       color: Colors.black,
+//                       fontSize: 28,
+//                     )),
+//               ),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   print('Você apertou o botão');
+//                 },
+//                 child: Text('Aperte o botão'),
+//               ),
+//             ],
+//           )),
